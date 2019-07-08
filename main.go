2f181139788e3bf6dc477d5f1301424abb0522f4
Spyros Trigazis <spyridon.trigazis@cern.ch>
2019-07-08 10:12:17 +0200

    Comment out mount/unmount examples
    Signed-off-by: Spyros Trigazis <spyridon.trigazis@cern.ch>

--- main.go
+++ main.go
@@ -52,12 +52,12 @@ func child() {
 	must(syscall.Chroot("/home/liz/ubuntufs"))
 	must(os.Chdir("/"))
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-	must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+	// must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
 
 	must(cmd.Run())
 
 	must(syscall.Unmount("proc", 0))
-	must(syscall.Unmount("thing", 0))
+	// must(syscall.Unmount("thing", 0))
 }
 
 func cg() {
